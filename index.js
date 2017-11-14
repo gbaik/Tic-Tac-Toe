15ac97f48db471d8ed65190b797d928ee84e9e8c
Gideon Baik <gideonbaik@gmail.com>
2017-11-13 21:29:10 -0800

    Add checkBoard and checkCoordinates

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var prompt = require('prompt');
 
 var board = [
-  [00, 01, 02],
-  [10, 11, 12],
-  [20, 21, 22]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
 ];
+
+var properCoordinates = {
+  '00' : true,
+  '01' : true, 
+  '02' : true, 
+  '10' : true, 
+  '11' : true, 
+  '12' : true, 
+  '20' : true, 
+  '21' : true, 
+  '22' : true
+  };
 
 prompt.start();
 
@@ -29,18 +41,47 @@ function playerX() {
   // Ask what position they want 
   prompt.get({
     properties: {
-      name: {
+      coordinates: {
         description: ("X, type a number to place")
       }
     }
   }, function (err, result) {
-    // Invoke check board function (results)
+    // Invoke check coordinate function (result) && check board function (result)  
+    if (!checkCoordinates(result.coordinates)) {
+      console.log('error1');
+      return;
+    }
+    // console.log(checkBoard(result.coordinates));
+    if (checkBoard(result.coordinates)){
       // Invoke error function (x)
+      console.log('error');
+    }
     // Else
       // Insert into board array
       // Display new board
       // Invoke winner function (x)
   });
+}
+
+// Check coordinate function
+function checkCoordinates (coordinates) {
+    // Check whether or not the value exists in the checkCoordinates array
+    return properCoordinates[coordinates];
+}
+
+// Check board function
+function checkBoard (coordinates) {
+  // Get first number
+  var first = coordinates[0];
+  // Get second number
+  var second = coordinates[1];
+  // return board[first][second];
+  var boardValue = board[first][second];
+  // If the value is either X or O
+    // return true
+  // Else
+    // return false
+  return boardValue === 'X' || boardValue === '0'
 }
 
 // O Player
@@ -51,14 +92,6 @@ function playerX() {
       // Insert into board array
       // Display new board
       // Invoke winner function (o)
-
-// Check board function
-  // Split arguments
-  // Use split string as index
-  // If the value is either X or O
-    // return true
-  // Else
-    // return false
 
 // Error function
   // Error message that it is not a valid move
